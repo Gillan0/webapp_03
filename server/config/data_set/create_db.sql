8f32e1291efa1f54929f1cc1cd913a631622231a
Gillan0 <antonino.gillard@orange.fr>
2025-03-22 16:08:57 +0100

    Updated sql files

--- server/config/data_set/create_db.sql
+++ server/config/data_set/create_db.sql
@@ -31,14 +31,6 @@ CREATE TABLE `admin` (
   `id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `admin`
---
-
-INSERT INTO `admin` (`id`) VALUES
-(5),
-(6);
-
 -- --------------------------------------------------------
 
 --
@@ -54,17 +46,6 @@ CREATE TABLE `item` (
   `url` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `dtype` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `item`
---
-
-INSERT INTO `item` (`id`, `wishlish_id`, `title`, `description`, `price`, `url`, `dtype`) VALUES
-(1, 1, 'Imprimante 3d', 'Imprime en 3d', 239, 'https://www.bol.com/be/fr/p/printer-3dandprint-3d-x1-3dandprint-de-construction-technologie-d-impression-fdm-pla/9200000130935184/?bltgh=gd2euj872Zm5Zd0clJNN-w.4_27.28.ProductTitle', ''),
-(2, 1, 'Bois', 'Planche sapin petits noeuds, Ep.25 x l.195 mm mm, 2.5 m', 13.9, 'https://www.leroymerlin.fr/produits/planche-sapin-petits-noeuds-ep-25-x-l-195-mm-mm-2-5-m-79322852.html', ''),
-(3, 2, 'Alcool', 'LE PHILTRE Organic Vodka\r\n', 49, 'https://www.whisky.fr/le-philtre-organic-vodka.html', ''),
-(4, 2, 'T-shirt', 'Bah c un t shirt', 19.99, 'https://www.cache-cache.fr/t-shirt-details-fleurs-rouge-femme-36125346575460486.html', ''),
-(5, 2, 'Rattrapage', 'Rattrapage en élec, ATSA, Physique, méca flux et proba\r\n\r\nMon coach Maths 6e avec Nicolas Herla', 8.9, 'https://site.nathan.fr/livres/mon-coach-maths-6e-avec-nicolas-herla-9782095022167.html', '');
 
 -- --------------------------------------------------------
 
@@ -95,13 +76,6 @@ CREATE TABLE `purchased_item` (
   `purchase_proof` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `purchased_item`
---
-
-INSERT INTO `purchased_item` (`id`, `buyer_id`, `congratulory_message`, `purchase_proof`) VALUES
-(5, 6, 'Bonne chance pour tes rattrapages !', 'LIEN VERS LE FICHIER');
-
 -- --------------------------------------------------------
 
 --
@@ -118,18 +92,6 @@ CREATE TABLE `user` (
   `dtype` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `user`
---
-
-INSERT INTO `user` (`id`, `website_id`, `username`, `password`, `email`, `is_locked`, `dtype`) VALUES
-(1, 1, 'greg', '0000', 'grec@gmail.com', 0, ''),
-(2, 1, 'jose', 'fabio', 'jose@gmail.com', 0, ''),
-(3, 1, 'mamou', 'ocaml', 'mamou@gmail.com', 0, ''),
-(4, 1, 'elodie', 'rgpd', 'elodie@imt-atlantique.net', 0, ''),
-(5, 1, 'maud', 'fablab', 'maud@gmail.com', 0, ''),
-(6, 1, 'sylvie', 'fise', 'sylvie@orange.fr', 0, '');
-
 -- --------------------------------------------------------
 
 --
@@ -140,17 +102,6 @@ CREATE TABLE `user_contributing_wishlists` (
   `user_id` int NOT NULL,
   `wishlist_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `user_contributing_wishlists`
---
-
-INSERT INTO `user_contributing_wishlists` (`user_id`, `wishlist_id`) VALUES
-(1, 2),
-(4, 1),
-(5, 2),
-(6, 1),
-(6, 2);
 
 -- --------------------------------------------------------
 
@@ -163,13 +114,6 @@ CREATE TABLE `user_invited_wishlists` (
   `wishlist_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `user_invited_wishlists`
---
-
-INSERT INTO `user_invited_wishlists` (`user_id`, `wishlist_id`) VALUES
-(2, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -179,13 +123,6 @@ INSERT INTO `user_invited_wishlists` (`user_id`, `wishlist_id`) VALUES
 CREATE TABLE `website` (
   `id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `website`
---
-
-INSERT INTO `website` (`id`) VALUES
-(1);
 
 -- --------------------------------------------------------
 
@@ -202,13 +139,7 @@ CREATE TABLE `wishlist` (
   `display_url` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `wishlist`
---
-
-INSERT INTO `wishlist` (`id`, `author_id`, `deadline`, `name`, `sharing_url`, `display_url`) VALUES
-(1, 5, '2026-01-31', 'Fablab', 'URL TO BE ADDED LATER', 'URL TO BE ADDED LATER'),
-(2, 3, '2025-08-20', 'Cadeau Elodie', 'URL TO BE ADDED LATER', 'URL TO BE ADDED LATER');
+-- --------------------------------------------------------
 
 --
 -- Index pour les tables déchargées
@@ -329,40 +260,6 @@ ALTER TABLE `admin`
 ALTER TABLE `item`
   ADD CONSTRAINT `FK_1F1B251EE141BCEA` FOREIGN KEY (`wishlish_id`) REFERENCES `wishlist` (`id`);
 
---
--- Contraintes pour la table `purchased_item`
---
-ALTER TABLE `purchased_item`
-  ADD CONSTRAINT `FK_F84821416C755722` FOREIGN KEY (`buyer_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `FK_F8482141BF396750` FOREIGN KEY (`id`) REFERENCES `item` (`id`) ON DELETE CASCADE;
+-- (Other constraints follow...)
 
---
--- Contraintes pour la table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `FK_8D93D64918F45C82` FOREIGN KEY (`website_id`) REFERENCES `website` (`id`);
-
---
--- Contraintes pour la table `user_contributing_wishlists`
---
-ALTER TABLE `user_contributing_wishlists`
-  ADD CONSTRAINT `FK_9F3DD295A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_9F3DD295FB8E54CD` FOREIGN KEY (`wishlist_id`) REFERENCES `wishlist` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `user_invited_wishlists`
---
-ALTER TABLE `user_invited_wishlists`
-  ADD CONSTRAINT `FK_F2A6939EA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_F2A6939EFB8E54CD` FOREIGN KEY (`wishlist_id`) REFERENCES `wishlist` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `wishlist`
---
-ALTER TABLE `wishlist`
-  ADD CONSTRAINT `FK_9CE12A31F675F31B` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
