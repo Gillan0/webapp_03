68f8cb18e26a4416cd886d82cba35a5a2c0adea4
user <user@imta.fr>
2025-03-25 23:00:09 +0100

    Implemented sorting of wishlist display items

--- server/src/Controller/SharingWishlistController.php
+++ server/src/Controller/SharingWishlistController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\WishlistRepository;
+
+use App\Entity\SortOrder;
+
 /**
  * @author Suzanne Veignant <suzanne.veignant@imt-atlantique.net>
  * @author Julien Abraul Guilherme "<julien.abraul-guilherme@imt-atlantique.net>
@@ -18,18 +21,28 @@ final class SharingWishlistController extends AbstractController
     {
         $this->wishlistRepository = $wishlistRepository;
     }
-    #[Route('{display_url}', name: 'app_sharing_wishlist')]
-    public function show(string $display_url): Response
+    #[Route('{display_url}/{sorted?}', name: 'app_sharing_wishlist', requirements: ["sorted" => "asc|desc"])]
+    public function show(string $display_url,
+                        ?string $sorted): Response
     
     {
+        if (empty($sorted)) {
+            $sorted = 'asc';
+        }
+
         $wishlist = $this->wishlistRepository->findOneBy(['displayUrl' => $display_url]);
 
         if (!$wishlist) {
             throw $this->createNotFoundException('Wishlist not found');
         }
 
+        $sortOrder = ($sorted == "asc") ? SortOrder::PriceAscending : SortOrder::PriceDescending;
+        $itemSorted = $wishlist->getSortedItems($sortOrder);
+
         return $this->render('sharing_wishlist/displayUrl.html.twig', [
-            'wishlist' => $wishlist
+            'wishlist' => $wishlist,
+            'sortedItems' => $itemSorted,
+            "otherOrder" => ($sorted=="asc") ? "desc" : "asc"
         ]);
     }
 }
